d6a28c5b300601340377a22fd6b1a5495655558d
Sohag <ahadulsohag@gmail.com>
2025-05-01 17:53:33 +0600

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,9 +1,11 @@
-function add(ta, i){
-    
-    return a;
+function nextInLine(arr, item){
+    arr.push(item);
+    return arr.shift();
 }
-
-var arr = [1, 22, 2];
-console.log("Before: "+ JSON.stringify(arr));
-console.log(add(arr, 6));
-console.log("After: "+ JSON.stringify(arr));
+var testArr = [1,2,3,4,5];
+console.log("Before: " + JSON.stringify(testArr));
+console.log(nextInLine(testArr, 6)); // Modify this line to test
+console.log("After: " + JSON.stringify(testArr));
+var ab = [1,2,3,4,5];
+console.log(ab);
+console.log(JSON.stringify(ab));
